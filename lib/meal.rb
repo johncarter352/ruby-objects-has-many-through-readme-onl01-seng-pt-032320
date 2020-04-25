de56520da4403280a6f51d62a0f61585553c6d24
John Carter <johncarter352@gmail.com>
2020-04-25 03:39:38 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -4,6 +4,13 @@ class Meal
   @@all = []
   
   def initialize(waiter, customer, total, tip=0)
-    
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+  
+  
 
 end
